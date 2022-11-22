352f02e691f15370e98f9658abf5df416ecbaf3f
Alexandre <costasalexandre511@gmail.com>
2022-11-22 17:56:27 -0300

    add home page, fix tailwind config, fix turbo config

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,5 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [require('@portfolio/styles/plugin')]
+  theme: {
+		fontSize: {
+			sm: 16,
+			md: 24,
+			lg: 32
+		},
+		colors: {
+			yellow: {
+				300: '#F0DB4F'
+			},
+			gray: {
+				200: '#ABB2BF',
+				500: '#767C87',
+				800: '#282C33',
+			},
+			white: '#FFFFFF'
+		},
+    fontFamily: {
+      'fira_code': ['"Fira Code"', 'sans-serif']
+    }
+  }
 }
